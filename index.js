fc0dd306f2bb4ac328a1aa02ce6ae249bfde3c8b
Hammad Ahmed <shammad.c5@hotmail.com>
2018-12-31 20:25:31 +0500

    parse the url

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const http = require('http');
+const url = require('url');
 
 const server = http.createServer((req, res) => {
+	const parsedUrl = url.parse(req.url, true);
+	const path = parsedUrl.pathname;
+	const trimmedPath = path.replace(/^\/+|\/+$/g, '');
 	res.end('Hello World!\n');
+
+	console.log(`Request received on path: ${trimmedPath}`);
 });
 
 server.listen(3000, () => console.log('The server is listening on port 3000'))
