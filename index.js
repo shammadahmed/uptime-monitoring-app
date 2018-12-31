66602149007f39d5db82d9681cda8b05be2b28c7
Hammad Ahmed <shammad.c5@hotmail.com>
2018-12-31 19:58:33 +0500

    add server

---
+++ index.js
@@ -0,0 +1,7 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+	res.end('Hello World!\n');
+});
+
+server.listen(3000, () => console.log('The server is listening on port 3000'))
