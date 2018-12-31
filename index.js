d7a7e451cd886313f20a60f2e175fb3d2d40377f
Hammad Ahmed <shammad.c5@hotmail.com>
2018-12-31 20:43:46 +0500

    obtain the method

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const http = require('http');
 const url = require('url');
 
 const server = http.createServer((req, res) => {
+
 	const parsedUrl = url.parse(req.url, true);
+
 	const path = parsedUrl.pathname;
 	const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+	const method = req.method.toLowerCase();
+
 	res.end('Hello World!\n');
 
-	console.log(`Request received on path: ${trimmedPath}`);
+	console.log(`Path: ${trimmedPath}; Method: ${method}`);
 });
 
 server.listen(3000, () => console.log('The server is listening on port 3000'))
