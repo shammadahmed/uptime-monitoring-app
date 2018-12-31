0d9a624c76d504c220e8a719c8ee85da7475014a
Hammad Ahmed <shammad.c5@hotmail.com>
2018-12-31 21:31:58 +0500

    obtain headers

--- index.js
+++ index.js
@@ -10,9 +10,14 @@ const server = http.createServer((req, res) => {
 
 	const method = req.method.toLowerCase();
 
+	const queryStringObject = parsedUrl.query;
+
+	const headers = req.headers;
+
 	res.end('Hello World!\n');
 
-	console.log(`Path: ${trimmedPath}; Method: ${method}`);
+	console.log(`Path: ${trimmedPath}; Method: ${method}; Query string object:`, queryStringObject);
+	console.dir(headers);
 });
 
 server.listen(3000, () => console.log('The server is listening on port 3000'))
